312591c4205adbaab06588648735027586f44171
Hibaraja21 <saadhiba05@gmail.com>
2023-04-17 15:00:14 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type of data: a dog struct
+ *
+ * @name: name of dog
+ * @age: dog's age
+ * @owner: name of owner
+ *
+ * description: just a lone dog struct in a big kitty world
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
